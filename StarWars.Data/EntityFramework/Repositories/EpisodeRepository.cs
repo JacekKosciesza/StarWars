008cb59575558cc95e87e7bf47e369ee6f8b7b45
Jacek Kościesza <jacek.kosciesza@gmail.com>
2017-03-11 08:48:28 +0100

    Droid repository CRUD unit tests

---
+++ StarWars.Data/EntityFramework/Repositories/EpisodeRepository.cs
@@ -0,0 +1,16 @@
+﻿using StarWars.Core.Models;
+using Microsoft.Extensions.Logging;
+using StarWars.Core.Data;
+
+namespace StarWars.Data.EntityFramework.Repositories
+{
+    public class EpisodeRepository : BaseRepository<Episode, int>, IEpisodeRepository
+    {
+        public EpisodeRepository() { }
+
+        public EpisodeRepository(StarWarsContext db, ILogger<EpisodeRepository> logger)
+            : base(db, logger)
+        {
+        }
+    }
+}
--- Tests/StarWars.Tests.Unit/Data/EntityFramework/Repositories/DroidRepositoryShould.cs
+++ Tests/StarWars.Tests.Unit/Data/EntityFramework/Repositories/DroidRepositoryShould.cs
@@ -4,6 +4,7 @@ using Moq;
 using StarWars.Core.Models;
 using StarWars.Data.EntityFramework;
 using StarWars.Data.EntityFramework.Repositories;
+using StarWars.Data.EntityFramework.Seed;
 using Xunit;
 
 namespace StarWars.Tests.Unit.Data.EntityFramework.Repositories
@@ -11,33 +12,110 @@ namespace StarWars.Tests.Unit.Data.EntityFramework.Repositories
     public class DroidRepositoryShould
     {
         private readonly DroidRepository _droidRepository;
+        private DbContextOptions<StarWarsContext> _options;
+        private Mock<ILogger<StarWarsContext>> _dbLogger;
         public DroidRepositoryShould()
         {
             // Given
-            var dbLogger = new Mock<ILogger<StarWarsContext>>();
+            _dbLogger = new Mock<ILogger<StarWarsContext>>();
             // https://docs.microsoft.com/en-us/ef/core/miscellaneous/testing/in-memory
-            var options = new DbContextOptionsBuilder<StarWarsContext>()
+            _options = new DbContextOptionsBuilder<StarWarsContext>()
                 .UseInMemoryDatabase(databaseName: "StarWars")
                 .Options;
-            using (var context = new StarWarsContext(options, dbLogger.Object))
+            using (var context = new StarWarsContext(_options, _dbLogger.Object))
             {
-                context.Droids.Add(new Droid { Id = 1, Name = "R2-D2" });
-                context.SaveChanges();
+                context.EnsureSeedData();
             }
-            var starWarsContext = new StarWarsContext(options, dbLogger.Object);
+            var starWarsContext = new StarWarsContext(_options, _dbLogger.Object);
             var repoLogger = new Mock<ILogger<DroidRepository>>();
             _droidRepository = new DroidRepository(starWarsContext, repoLogger.Object);
         }
 
         [Fact]
-        public async void ReturnR2D2DroidGivenIdOf1()
+        public async void ReturnR2D2DroidGivenIdOf2001()
         {
             // When
-            var droid = await _droidRepository.Get(1);
+            var droid = await _droidRepository.Get(2001);
 
             // Then
             Assert.NotNull(droid);
             Assert.Equal("R2-D2", droid.Name);
+        }
+
+        [Fact]
+        public async void AddNewDroid()
+        {
+            // Given
+            var droid2101 = new Droid { Id = 2101, Name = "Droid2101", PrimaryFunction = "Function2101" };
+
+            // When
+            _droidRepository.Add(droid2101);
+            var saved = await _droidRepository.SaveChangesAsync();
+
+            // Then
+            Assert.True(saved);
+            using (var db = new StarWarsContext(_options, _dbLogger.Object))
+            {
+                var droid = await db.Droids.FindAsync(2101);
+                Assert.NotNull(droid);
+                Assert.Equal(2101, droid.Id);
+                Assert.Equal("Droid2101", droid.Name);
+
+                // Cleanup
+                db.Droids.Remove(droid);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        [Fact]
+        public async void UpdateExistingDroid()
+        {
+            // Given
+            var threepio = await _droidRepository.Get(2000);
+            threepio.PrimaryFunction = "Function2000";
+
+            // When
+            _droidRepository.Update(threepio);
+            var saved = await _droidRepository.SaveChangesAsync();
+
+            // Then
+            Assert.True(saved);
+            using (var db = new StarWarsContext(_options, _dbLogger.Object))
+            {
+                var droid = await db.Droids.FindAsync(2000);
+                Assert.NotNull(droid);
+                Assert.Equal(2000, droid.Id);
+                Assert.Equal("Function2000", droid.PrimaryFunction);
+
+                // Cleanup
+                droid.PrimaryFunction = "Protocol";
+                db.Droids.Update(droid);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        [Fact]
+        public async void DeleteExistingDroid()
+        {
+            // Given
+            using (var db = new StarWarsContext(_options, _dbLogger.Object))
+            {
+                var droid2100 = new Droid { Id = 2100, Name = "Droid2100", PrimaryFunction = "Function2100" };
+                await db.Droids.AddAsync(droid2100);
+                await db.SaveChangesAsync();
+            }
+
+            // When
+            _droidRepository.Delete(2100);
+            var saved = await _droidRepository.SaveChangesAsync();
+
+            // Then
+            Assert.True(saved);
+            using (var db = new StarWarsContext(_options, _dbLogger.Object))
+            {
+                var deletedDroid = await db.Droids.FindAsync(2100);
+                Assert.Null(deletedDroid);
+            }
         }
     }
 }
